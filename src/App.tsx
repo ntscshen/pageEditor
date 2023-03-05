ff2f36957d2e42e8411ac928bd8f4d06cdd09b94
ntscshen <ntscshen@163.com>
2023-03-05 21:44:34 +0800

    fix: .env 内容配置错误

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <>还是React自由，vue用的浑身难受，一堆概念</>
+        <>React自由，vue一堆概念</>
         <ProjectList />
       </header>
     </div>
